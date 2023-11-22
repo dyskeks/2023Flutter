51512ae64f99c5b30f772dd576fc48526eb962f2
sju1026 <dyskeks100@gmail.com>
2023-11-22 12:52:54 +0900

    ch

---
+++ ch7/lib/Page1.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
+    );
+  }
+
+  //메뉴
+  Widget _buildTop(){
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('블랙'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('바이크'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('대리'),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: (){
+                print('클릭');
+              },
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('블랙'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('바이크'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('대리'),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMiddle(){
+    final imageItems = [
+      'assets/e1.jpg',
+      'assets/e2.jpg',
+      'assets/e3.jpg',
+    ];
+
+    return CarouselSlider( //사진 입력
+      options: CarouselOptions(
+        height: 500.0,
+      ),
+      items: imageItems.map((path)
+      {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              child: ClipRect(
+                child: Image.asset(
+                  path,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
+      }
+      ).toList(),
+    );
+  }
+
+  Widget _buildBottom(){
+    final items = List.generate(
+        10, (i){
+      return ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('공지사항입니다.'),
+      );
+    });
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
+  }
+}
